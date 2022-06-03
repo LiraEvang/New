222865dbf42eb5a79e49fd6c411e34f3f08497f5
Lira Mae <106497606+LiraEvang@users.noreply.github.com>
2022-06-03 15:54:37 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+		<html>
+		<head>
+			<center>
+				<font size ="8" color="#1E1620">
+					<b> <br> Registration Page <br> </br> </font>
+			</head>
+ 		<body bgcolor="#99A9AA">
+		<form>
+
+			<table>
+				<tr>
+					<td>
+						<font size ="5" color="#1E1620"> <b>
+
+		Username:
+
+		</td>
+		<td>
+			<input type="text" placeholder="Username" name="">
+		</td>
+		</tr>
+		<tr>
+		<td>	<br>
+				<font size="5" color="#1E1620"> <b>
+
+		Password:
+		
+		</td>
+		<td>	<br>
+				<input type="Password" placeholder="Password" name="">
+		</td>
+		</tr>
+		<tr>
+		<td>	<br>
+				<font size="5" color="#1E1620"> <b>
+
+		Email:
+		
+		</td>
+		<td>
+			<input type="mail" placeholder="Email" name="">
+		</td>
+		</tr>
+		<tr>
+		<td>	<br>
+				<font size="5" color="#1E1620"> <b>
+		
+		Birthdate:
+		
+		</td>
+		<td>
+			<input type="date" placeholder="Birthdate" name="">
+		</td>
+		</tr>
+		<tr>
+		<td>	<br>
+				<font size="5" color="#1E1620"> <b>
+
+			<form action="/action_page.php">
+  			
+		  	
+ 			 <input type="submit">
+			</form>
+
+
+
+		</body>
+		</html>
